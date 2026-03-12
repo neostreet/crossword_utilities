2acef4e807603e779848aa670ebf0b8ebaaa24f6
neostreet <lloyd.aidan@gmail.com>
2026-03-11 20:37:40 -0400

    minor refactor

--- grid_info.c
+++ grid_info.c
@@ -8,8 +8,28 @@ static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 static int count_blocks(char *line,int line_len);
+static int grid_info(char *filename);
 
 int main(int argc,char **argv)
+{
+  int retval;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  retval = grid_info(argv[1]);
+
+  if (retval) {
+    printf("grid_info of %s failed: %d\n",argv[1],retval);
+    return 2;
+  }
+
+  return 0;
+}
+
+static int grid_info(char *filename)
 {
   FILE *fptr;
   int line_len;
@@ -20,14 +40,9 @@ int main(int argc,char **argv)
   int total_blocks;
   double block_pct;
 
-  if (argc != 2) {
-    printf(usage);
+  if ((fptr = fopen(filename,"r")) == NULL) {
+    printf(couldnt_open,filename);
     return 1;
-  }
-
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
-    return 2;
   }
 
   line_no = 0;
@@ -45,7 +60,7 @@ int main(int argc,char **argv)
       save_line_len = line_len;
     else if (line_len != save_line_len) {
       printf("length of line %d doesn't conform\n",line_no);
-      return 3;
+      return 2;
     }
 
     blocks = count_blocks(line,line_len);
@@ -57,8 +72,7 @@ int main(int argc,char **argv)
   puzzle_size = save_line_len * line_no;
   block_pct = (double)total_blocks / (double)puzzle_size * (double)100;
 
-  printf("%d x %d\n",save_line_len,line_no);
-  printf("%6.2lf (%d %d)\n",block_pct,total_blocks,puzzle_size);
+  printf("%d x %d, %6.2lf (%d %d)\n",save_line_len,line_no,block_pct,total_blocks,puzzle_size);
 
   return 0;
 }
