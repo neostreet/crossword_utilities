cf7ec6b33722cc1fb78470b39c14863d81d8ce66
neostreet <lloyd.aidan@gmail.com>
2026-03-12 16:35:51 -0400

    refactor

--- print_words.c
+++ print_words.c
@@ -27,25 +27,20 @@ static char read_failed[] = "%s: read of %d bytes failed\n";
 static char word[MAX_WORD_LEN+1];
 static int word_len_counts[MAX_WORD_LEN-2];
 
-static void compress(char *in_buf,int num_lines,int line_len);
-static int do_across(char *in_buf,int num_lines,int line_len,bool bVerbose,int *num_letters_pt);
-static int do_down(char *in_buf,int num_lines,int line_len,bool bVerbose,int *num_letters_pt);
+static int read_grid(char *filename,char **in_buf_pt,int *width_pt,int *height_pt);
+static void compress(char *in_buf,int width,int height);
+static int do_across(char *in_buf,int width,int height,bool bVerbose,int *num_letters_pt);
+static int do_down(char *in_buf,int width,int height,bool bVerbose,int *num_letters_pt);
 
 int main(int argc,char **argv)
 {
-  int m;
   int n;
   int curr_arg;
   bool bVerbose;
-  struct stat statbuf;
-  off_t mem_amount;
+  int retval;
   char *in_buf;
-  int in_buf_ix;
-  int fhndl;
-  int bytes_to_io;
-  int num_lines;
-  int line_len;
-  int save_line_len;
+  int width;
+  int height;
   int total_words;
   int num_across_letters;
   int num_down_letters;
@@ -70,63 +65,12 @@ int main(int argc,char **argv)
     return 2;
   }
 
-  if (stat(argv[curr_arg],&statbuf) == -1) {
-    printf(couldnt_get_status,argv[curr_arg]);
-    return 3;
-  }
+  retval = read_grid(argv[curr_arg],&in_buf,&width,&height);
 
-  mem_amount = (size_t)statbuf.st_size;
+  compress(in_buf,height,width);
 
-  if ((in_buf = (char *)malloc(mem_amount)) == NULL) {
-    printf(malloc_failed,mem_amount);
-    return 3;
-  }
-
-  if ((fhndl = open(argv[curr_arg],O_BINARY | O_RDONLY,0)) == -1) {
-    printf(couldnt_open,argv[curr_arg]);
-    free(in_buf);
-    return 4;
-  }
-
-  bytes_to_io = (int)mem_amount;
-
-  if (read(fhndl,in_buf,bytes_to_io) != bytes_to_io) {
-    printf(read_failed,argv[curr_arg],bytes_to_io);
-    free(in_buf);
-    close(fhndl);
-    return 5;
-  }
-
-  if (bVerbose) {
-    for (n = 0; n < MAX_WORD_LEN - 2; n++)
-      word_len_counts[n] = 0;
-  }
-
-  num_lines = 0;
-  m = 0;
-
-  for (n = 0; n < bytes_to_io; n++) {
-    if (in_buf[n] == LINEFEED) {
-      line_len = n - m;
-      m = n + 1;
-      num_lines++;
-
-      if (num_lines == 1) {
-        save_line_len = line_len;
-        continue;
-      }
-
-      if (line_len != save_line_len) {
-        printf("length of line %d doesn't conform\n",num_lines);
-        return 3;
-      }
-    }
-  }
-
-  compress(in_buf,num_lines,save_line_len);
-
-  total_words = do_across(in_buf,num_lines,save_line_len,bVerbose,&num_across_letters);
-  total_words += do_down(in_buf,num_lines,save_line_len,bVerbose,&num_down_letters);
+  total_words = do_across(in_buf,width,height,bVerbose,&num_across_letters);
+  total_words += do_down(in_buf,width,height,bVerbose,&num_down_letters);
   total_letters = num_across_letters + num_down_letters;
 
   if (bVerbose)
@@ -139,21 +83,95 @@ int main(int argc,char **argv)
 
   free(in_buf);
 
+  return 0;
+}
+
+static int read_grid(char *filename,char **in_buf_pt,int *width_pt,int *height_pt)
+{
+  int m;
+  int n;
+  struct stat statbuf;
+  off_t mem_amount;
+  char *in_buf;
+  int in_buf_ix;
+  int fhndl;
+  int bytes_to_io;
+  int width;
+  int height;
+  int save_width;
+
+  if (stat(filename,&statbuf) == -1) {
+    printf(couldnt_get_status,filename);
+    return 1;
+  }
+
+  mem_amount = (size_t)statbuf.st_size;
+
+  if ((in_buf = (char *)malloc(mem_amount)) == NULL) {
+    printf(malloc_failed,mem_amount);
+    return 2;
+  }
+
+  if ((fhndl = open(filename,O_BINARY | O_RDONLY,0)) == -1) {
+    printf(couldnt_open,filename);
+    free(in_buf);
+    return 3;
+  }
+
+  bytes_to_io = (int)mem_amount;
+
+  if (read(fhndl,in_buf,bytes_to_io) != bytes_to_io) {
+    printf(read_failed,filename,bytes_to_io);
+    free(in_buf);
+    close(fhndl);
+    return 4;
+  }
+
+  for (n = 0; n < MAX_WORD_LEN - 2; n++)
+    word_len_counts[n] = 0;
+
+  height = 0;
+  m = 0;
+
+  for (n = 0; n < bytes_to_io; n++) {
+    if (in_buf[n] == LINEFEED) {
+      width = n - m;
+      m = n + 1;
+      height++;
+
+      if (height == 1) {
+        save_width = width;
+        continue;
+      }
+
+      if (width != save_width) {
+        printf("length of line %d doesn't conform\n",height);
+        free(in_buf);
+        close(fhndl);
+        return 5;
+      }
+    }
+  }
+
   close(fhndl);
+
+  *in_buf_pt = in_buf;
+  *width_pt = width;
+  *height_pt = height;
 
   return 0;
 }
 
-static void compress(char *in_buf,int num_lines,int line_len)
+static void compress(char *in_buf,int width,int height)
 {
   int m;
   int n;
   int p;
 
-  m = line_len;
-  n = line_len + 1;
+  m = width;
+  n = width + 1;
 
-  for (p = 0; p < (num_lines - 1) * line_len; p++) {
+  for (p = 0; p < (height - 1) * width; p++) {
     in_buf[m++] = in_buf[n++];
 
     if (in_buf[n] == LINEFEED)
@@ -161,7 +179,7 @@ static void compress(char *in_buf,int num_lines,int line_len)
   }
 }
 
-static int do_across(char *in_buf,int num_lines,int line_len,bool bVerbose,int *num_letters_pt)
+static int do_across(char *in_buf,int width,int height,bool bVerbose,int *num_letters_pt)
 {
   int m;
   int n;
@@ -176,11 +194,11 @@ static int do_across(char *in_buf,int num_lines,int line_len,bool bVerbose,int *
   num_words = 0;
   num_letters = 0;
 
-  for (m = 0; m < num_lines; m++) {
-    offset = m * line_len;
+  for (m = 0; m < height; m++) {
+    offset = m * width;
     bInWord = false;
 
-    for (n = 0; n < line_len; n++) {
+    for (n = 0; n < width; n++) {
       if (in_buf[offset + n] != '.') {
         if (!bInWord) {
           bInWord = true;
@@ -231,7 +249,7 @@ static int do_across(char *in_buf,int num_lines,int line_len,bool bVerbose,int *
   return num_words;
 }
 
-static int do_down(char *in_buf,int num_lines,int line_len,bool bVerbose,int *num_letters_pt)
+static int do_down(char *in_buf,int width,int height,bool bVerbose,int *num_letters_pt)
 {
   int m;
   int n;
@@ -245,17 +263,17 @@ static int do_down(char *in_buf,int num_lines,int line_len,bool bVerbose,int *nu
   num_words = 0;
   num_letters = 0;
 
-  for (m = 0; m < line_len; m++) {
+  for (m = 0; m < width; m++) {
     bInWord = false;
 
-    for (n = 0; n < num_lines; n++) {
-      if (in_buf[m + n * line_len] != '.') {
+    for (n = 0; n < height; n++) {
+      if (in_buf[m + n * width] != '.') {
         if (!bInWord) {
           bInWord = true;
           word_len = 0;
         }
 
-        word[word_len++] = in_buf[m + n * line_len];
+        word[word_len++] = in_buf[m + n * width];
       }
       else if (bInWord) {
         if (word_len > 1) {
