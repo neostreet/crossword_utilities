b9cc3a1ce0ff78217b2efef73b28db29d5a28872
neostreet <lloyd.aidan@gmail.com>
2026-08-26 12:39:22 -0400

    refactor read_grid()

--- unchecked_letters.c
+++ unchecked_letters.c
@@ -22,11 +22,14 @@ static char couldnt_get_status[] = "couldn't get status of %s\n";
 static char malloc_failed[] = "malloc of %d bytes failed\n";
 static char read_failed[] = "%s: read of %d bytes failed\n";
 
+#define MAX_WORD_LEN 20
+int word_len_counts[MAX_WORD_LEN-2];
+
 static int dbg_p;
 
 static int unchecked_letters(char *filename,int bVerbose);
-static int read_grid(char *filename,char **in_buf_pt,int *width_pt,int *height_pt);
-static void compress(char *in_buf,int width,int height);
+int read_grid(char *filename,char **in_buf_pt,int *width_pt,int *height_pt,int lower,int upper);
+void compress(char *in_buf,int width,int height);
 static int count_unchecked_letters(char *in_buf,int width,int height);
 static bool unchecked_letter(char *in_buf,int width,int height,int m,int n,int p);
 static void print_grid(char *grid,int width,int height);
@@ -74,7 +77,7 @@ static int unchecked_letters(char *filename,int bVerbose)
   int height;
   int num_unchecked_letters;
 
-  retval = read_grid(filename,&in_buf,&width,&height);
+  retval = read_grid(filename,&in_buf,&width,&height,0,0);
 
   if (retval) {
     printf("read_grid(() failed: %d\n",retval);
@@ -92,96 +95,6 @@ static int unchecked_letters(char *filename,int bVerbose)
   free(in_buf);
 
   return 0;
-}
-
-static int read_grid(char *filename,char **in_buf_pt,int *width_pt,int *height_pt)
-{
-  int m;
-  int n;
-  struct stat statbuf;
-  off_t mem_amount;
-  char *in_buf;
-  int in_buf_ix;
-  int fhndl;
-  int bytes_to_io;
-  int width;
-  int height;
-  int save_width;
-
-  if (stat(filename,&statbuf) == -1) {
-    printf(couldnt_get_status,filename);
-    return 1;
-  }
-
-  mem_amount = (size_t)statbuf.st_size;
-
-  if ((in_buf = (char *)malloc(mem_amount)) == NULL) {
-    printf(malloc_failed,mem_amount);
-    return 2;
-  }
-
-  if ((fhndl = open(filename,O_BINARY | O_RDONLY,0)) == -1) {
-    printf(couldnt_open,filename);
-    free(in_buf);
-    return 3;
-  }
-
-  bytes_to_io = mem_amount;
-
-  if (read(fhndl,in_buf,bytes_to_io) != bytes_to_io) {
-    printf(read_failed,filename,bytes_to_io);
-    free(in_buf);
-    close(fhndl);
-    return 4;
-  }
-
-  height = 0;
-  m = 0;
-
-  for (n = 0; n < bytes_to_io; n++) {
-    if (in_buf[n] == LINEFEED) {
-      width = n - m;
-      m = n + 1;
-      height++;
-
-      if (height == 1) {
-        save_width = width;
-        continue;
-      }
-
-      if (width != save_width) {
-        printf("length of line %d doesn't conform\n",height);
-        free(in_buf);
-        close(fhndl);
-        return 5;
-      }
-    }
-  }
-
-  close(fhndl);
-
-  *in_buf_pt = in_buf;
-  *width_pt = width;
-  *height_pt = height;
-
-  return 0;
-}
-
-static void compress(char *in_buf,int width,int height)
-{
-  int m;
-  int n;
-  int p;
-
-  m = width;
-  n = width + 1;
-
-  for (p = 0; p < (height - 1) * width; p++) {
-    in_buf[m++] = in_buf[n++];
-
-    if (in_buf[n] == LINEFEED)
-      n++;
-  }
 }
 
 static int count_unchecked_letters(char *in_buf,int width,int height)
