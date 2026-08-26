60be3333b5109fc51dd1abf1cd8ae7a8e46bf404
neostreet <lloyd.aidan@gmail.com>
2026-08-26 12:48:58 -0400

    refactor read_grid()

--- fgrid_info.c
+++ fgrid_info.c
@@ -25,10 +25,14 @@ static char couldnt_get_status[] = "couldn't get status of %s\n";
 static char malloc_failed[] = "malloc of %d bytes failed\n";
 static char read_failed[] = "%s: read of %d bytes failed\n";
 
+#define MAX_WORD_LEN 20
+static char word[MAX_WORD_LEN+1];
+int word_len_counts[MAX_WORD_LEN-2];
+
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 static int grid_info(char *filename,int terse_mode);
-static int read_grid(char *filename,char **in_buf_pt,int *width_pt,int *height_pt);
-static void compress(char *in_buf,int width,int height);
+int read_grid(char *filename,char **in_buf_pt,int *width_pt,int *height_pt,int lower,int upper);
+void compress(char *in_buf,int width,int height);
 static int count_blocks(char *in_buf,int puzzle_size);
 static int has_symmetry(char *in_buf,int puzzle_size);
 static int count_theme_letters(char *in_buf,int puzzle_size);
@@ -123,7 +127,7 @@ static int grid_info(char *filename,int terse_mode)
   int num_unchecked_letters;
   double unchecked_pct;
 
-  retval = read_grid(filename,&in_buf,&width,&height);
+  retval = read_grid(filename,&in_buf,&width,&height,0,0);
 
   if (retval) {
     printf("read_grid(() failed: %d\n",retval);
@@ -157,96 +161,6 @@ static int grid_info(char *filename,int terse_mode)
   free(in_buf);
 
   return 0;
-}
-
-static int read_grid(char *filename,char **in_buf_pt,int *width_pt,int *height_pt)
-{
-  int m;
-  int n;
-  struct stat statbuf;
-  off_t mem_amount;
-  char *in_buf;
-  int in_buf_ix;
-  int fhndl;
-  int bytes_to_io;
-  int width;
-  int height;
-  int save_width;
-
-  if (stat(filename,&statbuf) == -1) {
-    printf(couldnt_get_status,filename);
-    return 1;
-  }
-
-  mem_amount = (size_t)statbuf.st_size;
-
-  if ((in_buf = (char *)malloc(mem_amount)) == NULL) {
-    printf(malloc_failed,mem_amount);
-    return 2;
-  }
-
-  if ((fhndl = open(filename,O_BINARY | O_RDONLY,0)) == -1) {
-    printf(couldnt_open,filename);
-    free(in_buf);
-    return 3;
-  }
-
-  bytes_to_io = (int)mem_amount;
-
-  if (read(fhndl,in_buf,bytes_to_io) != bytes_to_io) {
-    printf(read_failed,filename,bytes_to_io);
-    free(in_buf);
-    close(fhndl);
-    return 4;
-  }
-
-  height = 0;
-  m = 0;
-
-  for (n = 0; n < bytes_to_io; n++) {
-    if (in_buf[n] == LINEFEED) {
-      width = n - m;
-      m = n + 1;
-      height++;
-
-      if (height == 1) {
-        save_width = width;
-        continue;
-      }
-
-      if (width != save_width) {
-        printf("length of line %d doesn't conform\n",height);
-        free(in_buf);
-        close(fhndl);
-        return 5;
-      }
-    }
-  }
-
-  close(fhndl);
-
-  *in_buf_pt = in_buf;
-  *width_pt = width;
-  *height_pt = height;
-
-  return 0;
-}
-
-static void compress(char *in_buf,int width,int height)
-{
-  int m;
-  int n;
-  int p;
-
-  m = width;
-  n = width + 1;
-
-  for (p = 0; p < (height - 1) * width; p++) {
-    in_buf[m++] = in_buf[n++];
-
-    if (in_buf[n] == LINEFEED)
-      n++;
-  }
 }
 
 static int count_blocks(char *in_buf,int puzzle_size)
